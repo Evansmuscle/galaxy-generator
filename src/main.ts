13192bcdf4672e3e369f277320155d981ce08216
Yiğit Kaan Korkmaz <77935633+Evansmuscle@users.noreply.github.com>
2022-04-06 20:48:48 +0300

    feat: finish the experience

---
+++ src/main.ts
@@ -0,0 +1,274 @@
+import * as THREE from "three";
+import * as dat from "dat.gui";
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+/**
+ * GUI
+ */
+const gui = new dat.GUI();
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+
+const starShape = textureLoader.load("/textures/particles/9.png");
+
+// Cursor
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (e) => {
+  cursor.x = e.clientX / sizes.width - 0.5;
+  cursor.y = e.clientY / sizes.height - 0.5;
+});
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Galaxy
+ */
+const parameters = {
+  count: 3000,
+  size: 0.02,
+  radius: 5,
+  branches: 3,
+  spin: 1,
+  randomness: 0.2,
+  randomnessPower: 3,
+  insideColor: "#ff6030",
+  outsideColor: "#1b3984",
+};
+
+let geometry: THREE.BufferGeometry | null = null;
+let material: THREE.PointsMaterial | null = null;
+let galaxy: THREE.Points | null = null;
+
+const generateGalaxy = () => {
+  if (geometry) {
+    geometry.dispose();
+  }
+
+  if (material) {
+    material.dispose();
+  }
+
+  if (galaxy) {
+    scene.remove(galaxy);
+  }
+
+  geometry = new THREE.BufferGeometry();
+
+  const vertices = new Float32Array(parameters.count * 3);
+  const colors = new Float32Array(parameters.count * 3);
+
+  const colorInside = new THREE.Color(parameters.insideColor);
+  const colorOutside = new THREE.Color(parameters.outsideColor);
+
+  for (let i = 0; i < parameters.count; i++) {
+    const i3 = i * 3;
+
+    // Position
+    const radius = Math.random() * parameters.radius;
+
+    const spinAngle = radius * parameters.spin;
+    const branchAngle =
+      ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+
+    const randomX =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? 1 : -1) *
+      parameters.randomness *
+      radius;
+    const randomY =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? 1 : -1) *
+      parameters.randomness *
+      radius;
+    const randomZ =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? 1 : -1) *
+      parameters.randomness *
+      radius;
+
+    vertices[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius + randomX;
+    vertices[i3 + 1] = 0 + randomY;
+    vertices[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ;
+
+    // Color
+    const mixedColor = colorInside.clone();
+    mixedColor.lerp(colorOutside, radius / parameters.radius);
+
+    colors[i3 + 0] = mixedColor.r;
+    colors[i3 + 1] = mixedColor.g;
+    colors[i3 + 2] = mixedColor.b;
+  }
+
+  const positionAttr = new THREE.BufferAttribute(vertices, 3);
+  const colorAttr = new THREE.BufferAttribute(colors, 3);
+
+  geometry.setAttribute("position", positionAttr);
+  geometry.setAttribute("color", colorAttr);
+
+  material = new THREE.PointsMaterial({
+    size: parameters.size,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    alphaMap: starShape,
+    transparent: true,
+    vertexColors: true,
+  });
+
+  galaxy = new THREE.Points(geometry, material);
+
+  scene.add(galaxy);
+};
+
+generateGalaxy();
+
+gui
+  .add(parameters, "count")
+  .min(100)
+  .max(1000000)
+  .step(100)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "size")
+  .min(0.001)
+  .max(0.1)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "radius")
+  .min(0.01)
+  .max(20)
+  .step(0.01)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "branches")
+  .min(2)
+  .max(20)
+  .step(1)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "spin")
+  .min(-5)
+  .max(5)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "randomness")
+  .min(0)
+  .max(2)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "randomnessPower")
+  .min(1)
+  .max(10)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+
+gui.addColor(parameters, "insideColor").onFinishChange(generateGalaxy);
+gui.addColor(parameters, "outsideColor").onFinishChange(generateGalaxy);
+
+// Camera
+
+const canvas = document.createElement("canvas");
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+
+window.addEventListener("resize", (_) => {
+  // Update Sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update Camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update Renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+window.addEventListener("dblclick", (_) => {
+  const doc: any = window.document;
+  const fullscreenElement =
+    doc.fullscreenElement || doc.webkitFullscreenElement;
+
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+      //@ts-ignore
+    } else if (canvas.webkitRequestFullscreen) {
+      //@ts-ignore
+      canvas.webkitRequestFullscreen();
+    }
+    return;
+  }
+
+  if (doc.exitFullscreen) {
+    doc.exitFullscreen();
+    return;
+  }
+
+  if (doc.webkitExitFullscreen) {
+    doc.webkitExitFullscreen();
+  }
+});
+
+camera.position.set(0, 0, 3);
+
+scene.add(camera);
+
+// Controls
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// Renderer
+
+document.body.appendChild(canvas);
+
+const rendererOptions: THREE.WebGLRendererParameters = {
+  canvas,
+};
+
+const renderer = new THREE.WebGLRenderer(rendererOptions);
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Animations
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update Camera Via Orbit Controls
+
+  controls.update();
+
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
